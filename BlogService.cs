a227ca02ca432cf6d291515ff694f138d49d6e55
Elena Ivanova <elenaplehanovabda@gmail.com>
2023-03-19 23:23:19 +0700

    #1 - Added 3 queries

---
+++ BlogService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+internal class BlogService
+{
+    internal static object NumberOfCommentsPerUser(MyDbContext context) =>
+       context.BlogComments.GroupBy(b => b.UserName).Select(g => new { Name = g.Key, Count = g.Count() }).ToList();
+
+
+    internal static object NumberOfLastCommentsLeftByUser(MyDbContext context) =>
+        context.BlogPosts.Select(b => new
+        {
+            LastComment = b.Comments.OrderBy(c => c.CreatedDate).Select(x => new { x.UserName }).Last()
+        }).GroupBy(l => l.LastComment.UserName).Select(g => new { Name = g.Key, Count = g.Count() }).ToList();
+
+    internal static object PostsOrderedByLastCommentDate(MyDbContext context) =>
+        context.BlogPosts.Select(b => new
+        {
+            b.Title,
+            LastComment = b.Comments.OrderBy(c => c.CreatedDate).Select(x => new { Date = x.CreatedDate.ToString("yyyy-MM-dd"), x.Text }).Last()
+        }).ToList();
+}
